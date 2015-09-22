5d9e6cefa765f10a716819238c1768f24816db7c
wei zhang <charleyhuman@gmail.com>
2015-09-22 07:25:44 -0500

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort_by])
+#    session.clear
+    @all_ratings = Movie.select('DISTINCT rating').order('rating ASC').collect {|result| result.rating}#Movie.ratings
+#    @movies = Movie.order(params[:sort_by])#Movie.all
+ 
+    filters = {:sort_by => "", :ratings => {"G" => "1", "R" => "1", "PG" => "1", "PG-13" => "1", "NC-17" => "1"}}
+    redirect = false
+    filters.each do |filter, default|
+      if params[filter].blank?
+#        if !session[filter].blank?
+#          redirect = true
+#          params[filter] = session[filter]
+#        else
+          params[filter] = default
+#        end
+      end
+#      session[filter] = params[filter]
+    end
+#    puts '1' + session[:sort_by].to_s
+#    puts '2' + session[:ratings].keys.to_s
+    redirect_to movies_path(:sort_by => params[:sort_by], :ratings => params[:ratings]) if redirect
+    condition = params[:ratings].keys
+    order = params[:sort_by]
+#    puts '3' + condition.to_s
+#    puts '4' + order.to_s
+    @movies = Movie.where("rating": condition).order(params[:sort_by])
+#    @movies = Movie.order(params[:sort_by])
   end
 
   def new
